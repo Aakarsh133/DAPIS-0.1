bd6b1a6415f581ce40313d69e0f666481ae17f6c
Aakarsh Maurya <mauryaaakarsh11@gmail.com>
2023-12-30 18:27:48 +0530

    Above functionalities are successfully deployed in main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd 
 import numpy as np 
 import random as r
-from Frontend import Welcome_screen
-from Backend import dataset_importer
+from Frontend import Welcome_screen, Menu1
+#from Backend import dataset_importer
 
 Welcome_screen.animation()
+df= Menu1.men1()
+print("\nThe presented database is:- \n")
+print(df.head())
 
