7da6f4e5944aca3311ffda089ac2ae0a6cc090d3
Aakarsh Maurya <mauryaaakarsh11@gmail.com>
2023-12-30 13:27:41 +0530

    main.py is currenlty bug free and smoothly loading intro screen

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np 
 import random as r
 from Frontend import Welcome_screen
-from Backend.dataset_importer import importer
+from Backend import dataset_importer
 
-importer()
+Welcome_screen.animation()
 
