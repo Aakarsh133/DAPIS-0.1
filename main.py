53698fce95acc0a60e89a51e9e971abbf7262288
Aakarsh Maurya <mauryaaakarsh11@gmail.com>
2023-12-30 12:56:56 +0530

    want to implement smooth file import <check issues>

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd 
 import numpy as np 
 import random as r
-from Backend import dataset_importer
 from Frontend import Welcome_screen
+from Backend.dataset_importer import importer
 
+importer()
 
-
-dataset_importer.importer
