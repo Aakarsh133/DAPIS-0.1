4b2da171ea448a4ac1b6e6abb77f4ae317de656d
Aakarsh Maurya <mauryaaakarsh11@gmail.com>
2023-12-30 13:27:02 +0530

    __init__.py is back here as well and works fine

--- Frontend/Welcome_screen.py
+++ Frontend/Welcome_screen.py
@@ -1,99 +1,101 @@
 import time
 import sys
 
-def anim1():
+def animation():
+    def anim1():
 
-    frame = ["....", "."]
+        frame = ["....", "."]
 
-    for i in frame:
-        sys.stdout.write(i)
-        sys.stdout.flush()
-        time.sleep(0.25)
-
-
-    def anm2():
-
-        sys.stdout.write(frame[1])
-        sys.stdout.flush()
-        time.sleep(0.25)
-        sys.stdout.write("\b \b")
-        sys.stdout.flush()
-        time.sleep(0.25)
-
-    for i in range(3):
-        anm2()
-
-    print("\t", end= "")
-
-def anim2():
-    frames = [
-    "⠋",
-    "⠙",
-    "⠹",
-    "⠸",
-    "⠼",
-    "⠴",
-    "⠦",
-    "⠧",
-]
-    
-    for i in range(3):
-
-        for i in frames:
-            sys.stdout.write("\b"+i)
-            sys.stdout.flush()
-            time.sleep(0.1)
-    
-    print("\t", end= "")
-    
-
-def anim3():
-
-    frames= ['D', 'A', 'P', 'I', 'S', '-','0','.','1',' ','W', 'E', 'L', 'C', 'O', 'M', 'E', 'S', ' ', 'T', 'H', 'E', ' ', 'U', 'S', 'E', 'R']
-    for i in frames:
-
-        sys.stdout.write(i)
-        sys.stdout.flush()
-        time.sleep(0.10)
-
-    print("\t", end= "")
-
-def anim4():
-
-    frames= ['(', '^', '_', '^', ')']
-    for j in range(2):
-        for i in frames:
+        for i in frame:
             sys.stdout.write(i)
             sys.stdout.flush()
-            time.sleep(0.15)
+            time.sleep(0.25)
 
-    print("\t", end= "")
 
-def anim5():
+        def anm2():
 
-    frames = [
-    "⠋",
-    "⠙",
-    "⠹",
-    "⠸",
-    "⠼",
-    "⠴",
-    "⠦",
-    "⠧",
-]
-    
-    for i in range(3):
-
-        for i in range(len(frames)-1, 0, -1):
-            sys.stdout.write("\b"+frames[i])
+            sys.stdout.write(frame[1])
             sys.stdout.flush()
-            time.sleep(0.1)
-    
-    print("\t", end= "")
-    
-    
-anim1()
-anim2()
-anim3()
-anim4()
-anim5()
+            time.sleep(0.25)
+            sys.stdout.write("\b \b")
+            sys.stdout.flush()
+            time.sleep(0.25)
+
+        for i in range(3):
+            anm2()
+
+        print("\t", end= "")
+
+    def anim2():
+        frames = [
+            "⠋",
+            "⠙",
+            "⠹",
+            "⠸",
+            "⠼",
+            "⠴",
+            "⠦",
+            "⠧",
+        ]
+            
+        for i in range(3):
+
+            for i in frames:
+                sys.stdout.write("\b"+i)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            
+        print("\t", end= "")
+            
+
+    def anim3():
+
+        frames= ['D', 'A', 'P', 'I', 'S', '-','0','.','1',' ','W', 'E', 'L', 'C', 'O', 'M', 'E', 'S', ' ', 'T', 'H', 'E', ' ', 'U', 'S', 'E', 'R']
+        for i in frames:
+
+            sys.stdout.write(i)
+            sys.stdout.flush()
+            time.sleep(0.10)
+
+        print("\t", end= "")
+
+    def anim4():
+
+        frames= ['(', '^', '_', '^', ')']
+        for j in range(2):
+            for i in frames:
+                sys.stdout.write(i)
+                sys.stdout.flush()
+                time.sleep(0.15)
+
+        print("\t", end= "")
+
+    def anim5():
+
+        frames = [
+            "⠋",
+            "⠙",
+            "⠹",
+            "⠸",
+            "⠼",
+            "⠴",
+            "⠦",
+            "⠧",
+    ]
+            
+        for i in range(3):
+
+            for i in range(len(frames)-1, 0, -1):
+                sys.stdout.write("\b"+frames[i])
+                sys.stdout.flush()
+                time.sleep(0.1)
+            
+        print("\t", end= "")
+
+    anim1()
+    anim2()
+    anim3()
+    anim4()
+    anim5()
+        
+        
