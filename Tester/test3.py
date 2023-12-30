1dd2e9827cc7f932abd55277fb82f633c7d0c50b
Aakarsh Maurya <mauryaaakarsh11@gmail.com>
2023-12-30 18:30:15 +0530

    Tested more functionalities before deployement.

---
+++ Tester/test3.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df= pd.read_excel('/home/aakarsh/Downloads/file_example_XLS_10.xls')
---
+++ Tester/test4.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import numpy as np 
+import random as r
+from Frontend import Welcome_screen, Menu1
+from Backend import dataset_importer
+
+
+df= Menu1.men1()
+print(df.head())
